f9991f52dd68c1c4bf4baa774664edb8cea515d6
XXZ <108875830+ThisisXXZ@users.noreply.github.com>
2025-03-03 11:35:20 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+touch ~/Documents/file1.bat ~/Documents/file3.bat ~/Documents/file4.bat
+dd if=/dev/zero of=~/Documents/file5.dat bs=1M count=20
+dd if=/dev/zero of=~/Documents/file2.dat bs=1M count=15
