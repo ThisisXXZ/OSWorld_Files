9130eb90fd773d9b036eb3b462edfa176cf0be89
XXZ <108875830+ThisisXXZ@users.noreply.github.com>
2025-03-03 11:40:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-touch ~/Documents/file1.bat ~/Documents/file3.bat ~/Documents/file4.bat
+touch ~/Documents/file1.bat ~/Documents/file2.bat ~/Documents/file3.bat ~/Documents/file4.bat ~/Documents/file5.bat
 dd if=/dev/zero of=~/Documents/file5.dat bs=1M count=20
 dd if=/dev/zero of=~/Documents/file2.dat bs=1M count=15
